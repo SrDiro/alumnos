4f162e623d2d24ca18502860ca3dc7461aef6820
rasul <rasul@192.168.1.244>
2017-09-29 00:03:36 +0200

    v.1.0

---
+++ public_html/alumnos.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function crearAlumno() {
+    this.nombreAlumno = document.getElementById("nombreAlumno").value;
+    this.edadAlumno = document.getElementById("edadAlumno").value;
+    this.ciudadAlumno = document.getElementById("ciudadAlumno").value;
+}
---
+++ public_html/grupos.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function crearGrupo() {
+    this.nombreGrupo = document.getElementById("nombreGrupo").value;
+    this.anoGrupo = document.getElementById("anoGrupo").value;
+    this.alumnosMatriculados = [];
+
+}
+
+crearGrupo.prototype.matricular = function (Alumno) {
+    this.alumnosMatriculados.push(Alumno);
+}
